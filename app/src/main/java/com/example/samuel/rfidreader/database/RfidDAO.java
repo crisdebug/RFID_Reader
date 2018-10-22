8d3476a3609e457939590933bb120dbd963bc98b
Rafhael Gaspar <rafhael2gaspar@gmail.com>
2018-10-22 13:30:53 -0300

    Alterado 2210

---
+++ app/src/main/java/com/example/samuel/rfidreader/database/RfidDAO.java
@@ -0,0 +1,11 @@
+package com.example.samuel.rfidreader.database;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class RfidDAO {
+
+    private SQLiteOpenHelper openHelper;
+    private SQLiteDatabase database;
+
+}
